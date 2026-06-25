18a774879181b72c96e7f077b91ba4e7a478c79f
Paul Deon Foli <astrosteck@gmail.com>
2026-06-25 13:51:58 +0000

    Added the short description.

--- MY-PORTFOLIO/src/HomePage/Hompage.jsx
+++ MY-PORTFOLIO/src/HomePage/Hompage.jsx
@@ -9,7 +9,7 @@ export function Homepage() {
         </div>
         <h2>EPISILION_SERVICES</h2>
         <div className="homepage-opening-description">
-          <p>WEB AND APP DEVELOPER (UI/UX)</p> 
+          <p>WEB AND APP DEVELOPER (UI/UX)</p>
           {/* <span class="cursor-pipe">|</span> */}
         </div>
 
@@ -17,6 +17,55 @@ export function Homepage() {
           <div class="note long"></div>
           <div class="note short"></div>
           <div class="note long"></div>
+        </div>
+      </div>
+
+      {/* <div className="short-description-container">
+        <div>
+          <p>Network Uptime</p>
+          <p>99.9%</p>
+        </div>
+      </div> */}
+
+      <div class="hud-container">
+        <div class="hud-card">
+          <div class="corner top-left"></div>
+          <div class="corner bottom-right"></div>
+
+          <div class="hud-content">
+            <div class="hud-title">NETWORK UPTIME</div>
+            <div class="hud-value">99.9%</div>
+          </div>
+        </div>
+
+        <div class="hud-card">
+          <div class="corner top-left"></div>
+          <div class="corner bottom-right"></div>
+
+          <div class="hud-content">
+            <div class="hud-title">NETWORK UPTIME</div>
+            <div class="hud-value">99.9%</div>
+          </div>
+        </div>
+
+        <div class="hud-card">
+          <div class="corner top-left"></div>
+          <div class="corner bottom-right"></div>
+
+          <div class="hud-content">
+            <div class="hud-title">NETWORK UPTIME</div>
+            <div class="hud-value">99.9%</div>
+          </div>
+        </div>
+
+        <div class="hud-card">
+          <div class="corner top-left"></div>
+          <div class="corner bottom-right"></div>
+
+          <div class="hud-content">
+            <div class="hud-title">NETWORK UPTIME</div>
+            <div class="hud-value">99.9%</div>
+          </div>
         </div>
       </div>
     </>
